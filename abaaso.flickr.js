5439db22fcbafff61a3d7ddf1c8bf6b50fbafecd
Jason Mulligan <jason.mulligan@avoidwork.com>
2011-12-09 16:57:32 -0500

    Finalized the 1.0 release without a grid, keyboard listener is active

--- abaaso.flickr.js
+++ abaaso.flickr.js
@@ -9,7 +9,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Jason Mulligan nor the
+ *     * Neither the name of abaaso.flickr nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
@@ -28,13 +28,10 @@
 /**
  * abaaso.flickr
  *
- * Note: Internet Explorer 8 is not supported
- *
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
  * @link http://avoidwork.com
- * @requires abaaso 1.7.010
- * @requires abaaso.fx 1.1
- * @version 1.0.tech
+ * @requires abaaso 1.7.5
+ * @version 1.0
  */
 $.on("init", function(){
 	$.module("flickr", {
@@ -63,15 +60,12 @@ $.on("init", function(){
 			var  self  = this,
 			     r     = self.data.get(index).data,
 			     obj   = $("#photo"),
-			     img   = $.create("img", {style:"display:none;", src: r.sizes.last().source}, obj),
-			     cover = $("#cover");
+			     img   = $.create("img", {style:"display:none;", src: r.sizes.last().source}, obj);
 
 			obj.css("opacity", 0);
-			cover.addClass("loading");
 
 			img.on("load", function(){
 				this.un("load");
-				cover.removeClass("loading");
 				obj.style.backgroundImage = "url(" + this.src + ")";
 				this.destroy();
 				obj.css("opacity", 1);
@@ -80,47 +74,11 @@ $.on("init", function(){
 		},
 
 		/**
-		 * Displays or hides the image grid
-		 *
-		 * @param {Boolean} visible  Display or hide the grid
-		 * @param {Object} Image grid
-		 */
-		grid : function(visible) {
-			var obj  = typeof $("#grid") !== "undefined" ? $("#grid") : $.create("div", {id: "grid", "class" : "right"}).hide(),
-			    self = this,
-			    anchor, thumb, i, nth;
-
-			self.grid.resize = function() {
-				if (/left|right/.test(this.className)) this.style.height = ($.client.size.y - 75) + "px"
-			}
-
-			if (obj.childNodes.length === 0 && !self.data.total > 0) {
-				this.grid.resize.call($("#grid"));
-				$.on("resize", function(){ this.grid.resize.call($("#grid")); }, "grid", this)
-
-				nth = self.data.total;
-				for (i = 0; i < nth; i++) {
-					obj.create("a", {id: "grid-" + i})
-					   .on("click", function(){
-					   		self.config.photo = parseInt(this.id.replace(/grid-/, ""));
-					   		self.display(this.id.replace(/grid-/, ""));
-						})
-					   .create("img", {src: self.data.get(i).data.sizes.first().source});
-				}
-			}
-
-			visible ? obj.show() : obj.hide();
-			return obj;
-		},
-
-		/**
 		 * Retrieves a photoset from Flickr
 		 */
 		init : function() {
-			if (this.config.id === null || this.config.key === null || ($.client.ie && $.client.version == 8))
+			if (this.config.id === null || this.config.key === null)
 					throw Error($.label.error.invalidArguments);
-
-			cover.addClass("loading");
 
 			var self = this,
 			    i    = (self.config.sets.length > 1) ? Math.floor(Math.random() * self.config.sets.length + 1) : 0,
@@ -128,33 +86,29 @@ $.on("init", function(){
 			    key, fn, index;
 
 			self.config.loaded = self.config.sets[i];
+
 			if (typeof $("#year") !== "undefined") $("#year").text(new Date().getFullYear());
 
-			// UX listeners
-			switch (true) {
-				case $.client.mobile || $.client.tablet:
-					// $("#nav").hide();
-					// break;
-				default:
-					$("#next").on("click", function(){ this.next(); }, "next", this);
-					$("#prev").on("click", function(){ this.prev(); }, "prev", this);
-					$("#slideshow").on("click", function(){
-						switch (true) {
-							case this.config.slide:
-								$("#slideshow").update({innerHTML: "Start"});
-								this.config.slide = false;
-								clearTimeout(this.config.timer);
-								break;
-							default:
-								$("#slideshow").update({innerHTML: "Stop"});
-								this.config.slide = true;
-								this.next();
-						}
-					}, "slideshow", this);
-			}
+			// UI listeners
+			$.on(document, "keydown", this.key, "keyboard", this);
+			$("#next").on("click", this.next, "next", this);
+			$("#prev").on("click", this.prev, "prev", this);
+			$("#slideshow").on("click", function(){
+				switch (true) {
+					case this.config.slide:
+						$("#slideshow").update({innerHTML: "Start"});
+						this.config.slide = false;
+						clearTimeout(this.config.timer);
+						break;
+					default:
+						$("#slideshow").update({innerHTML: "Stop"});
+						this.config.slide = true;
+						this.next();
+				}
+			}, "slideshow", this);
 
+			// Setting up a data store
 			$.store(this);
-
 			this.on("afterDataSet", function(r) { this.load(r, false); }, "photo")
 			    .on("afterDataSync", function(data) {
 			    	var o = this.parentNode;
@@ -170,10 +124,13 @@ $.on("init", function(){
 
 		/**
 		 * Keypress handler
+		 * 
+		 * @param {Object} e Keyboard event
+		 * @return {Object} Keyboard event
 		 */
 		key : function(e) {
-			var unicode = (e.keyCode) ? e.keyCode : e.charCode;
-			switch (unicode) {
+			var code = (e.keyCode) ? e.keyCode : e.charCode;
+			switch (code) {
 				case 37:
 				case 40:
 					this.prev();
@@ -194,6 +151,8 @@ $.on("init", function(){
 		 * @return {Object} Photo record to display
 		 */
 		load : function(photo, display) {
+			if (typeof photo === "undefined" || typeof photo.key === "undefined") return this.next();
+
 			display   = (display !== false);
 			var uri   = "http://api.flickr.com/services/rest/?&method=flickr.photos.getSizes&api_key=" + this.config.key + "&photo_id=" + photo.key + "&format=json&jsoncallback=?",
 			    index = this.data.keys[photo.key].index,
@@ -237,6 +196,6 @@ $.on("init", function(){
 			return i;
 		},
 
-		version : "1.0.beta"
+		version : "1.0"
 	});
 }, "abaaso.flickr");
