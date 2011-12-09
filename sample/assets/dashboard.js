4daae96ea6faa63d838e8156860470856fc1218f
Jason Mulligan <jason.mulligan@avoidwork.com>
2011-12-09 17:05:05 -0500

    Returning sample dashboard to original state (unpopulated)

--- sample/assets/dashboard.js
+++ sample/assets/dashboard.js
@@ -1,16 +1,10 @@
 $.on("render", function(){
-	switch (true) {
-		case this.client.mobile:
-		case this.client.table:
-			return undefined;
-	}
-
 	// Setting up Flickr
-	this.flickr.config.id      = "63182296@N00";
-	this.flickr.config.key     = "38d4dace912074353e60918d64aac446";
-	this.flickr.config.sets    = ["72157626821186687"];
+	this.flickr.config.id      = "";
+	this.flickr.config.key     = "";
+	this.flickr.config.sets    = [];
 	this.flickr.config.timeout = 10000;
 
 	// Uncomment the line below
-	this.flickr.init();
+	//this.flickr.init();
 });
