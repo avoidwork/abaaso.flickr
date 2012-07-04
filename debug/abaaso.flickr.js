7542d85bbb44527575340403a050ced4e36515f5
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-07-03 20:16:05 -0400

    Initial refactoring to be a factory with no GUI concerns

--- debug/abaaso.flickr.js
+++ debug/abaaso.flickr.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011, Jason Mulligan <jason.mulligan@avoidwork.com>
+ * Copyright (c) 2011 - 2012, Jason Mulligan <jason.mulligan@avoidwork.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,219 +30,101 @@
  *
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
  * @link http://avoidwork.com
- * @requires abaaso 1.9
- * @version 1.3
+ * @requires abaaso 2.4.1
+ * @version 1.4.beta
  */
-(function (window) {
+(function (global) {
 	"use strict";
 
-	var flickr = (function () {
-		var $ = window[abaaso.aliased],
-		    config, display, init, key, load, next, prev;
+	var $, flickr, init;
 
-		config = {
-			data    : {},
-			id      : null,
-			key     : null,
-			photo   : null,
-			loaded  : null,
-			timer   : null,
-			timeout : 15000,
-			sets    : [""],
-			slide   : false,
-			version : "1.3"
-		};
 
-		/**
-		 * Displays a photo from the set
-		 *
-		 * @param {Integer} index  Index of photo
-		 */
-		display = function (index) {
-			if (index != config.photo) return;
+	/**
+	 * Flickr factory
+	 * 
+	 * @param  {Object} config [Optional] Configuration to apply
+	 * @return {Object}        Instance of flickr
+	 */
+	flickr = function (config) {
+		var self     = this;
+		self.id      = null;
+		self.key     = null;
+		self.loaded  = null;
+		self.photo   = null;
+		self.sets    = [];
+		self.timer   = null;
+		self.timeout = 15000;
+		if (config instanceof Object) $.iterate(config, function (v, k) { self[k] = v; });
+		return self;
+	};
 
-			var  self  = $.flickr,
-			     r     = self.data.get(index).data,
-			     obj   = $("#photo"),
-			     img   = $.create("img", {style:"display:none;", src: r.sizes.last().source}, obj);
+	/**
+	 * Retrieves a photoset from Flickr
+	 */
+	flickr.prototype.init = function () {
+		if (this.id === null || this.key === null) throw Error($.label.error.invalidArguments);
 
-			obj.css("opacity", 0);
+		var self = self,
+		    i    = (self.sets.length > 1) ? Math.floor(Math.random() * self.sets.length + 1) : 0,
+		    uri  = "http://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=" + self.key + "&photoset_id=" + self.sets[i] + "&format=json&jsoncallback=?",
+		    fn;
 
-			img.on("load", function () {
-				this.un("load");
-				obj.css("background-image", "url(" + this.src + ")");
-				this.destroy();
-				obj.css("opacity", 1);
-			});
-
-			if (typeof $.timer.flickr !== "undefined") $.timer.flickr = setTimeout(function () { next(); }, $.flickr.config.timeout);
-		};
-
-		/**
-		 * Retrieves a photoset from Flickr
-		 */
-		init = function () {
-			if (config.id === null || config.key === null)
-					throw Error($.label.error.invalidArguments);
-
-			var self = $.flickr,
-			    i    = (config.sets.length > 1) ? Math.floor(Math.random() * config.sets.length + 1) : 0,
-			    uri  = "http://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=" + config.key + "&photoset_id=" + config.sets[i] + "&format=json&jsoncallback=?",
-			    fn, index;
-
-			// Click & spacebar handler
-			fn = function () {
-				switch (true) {
-					case typeof $.timer.flickr !== "undefined":
-						clearTimeout($.timer.flickr);
-						delete $.timer.flickr;
-						break;
-					default:
-						$.timer.flickr = null;
-						next();
-				};
-			};
-
-			config.loaded = config.sets[i];
-
-			if (typeof $("#year") !== "undefined") $("#year").text(new Date().getFullYear());
-
-			// UI listeners
-			$.on(document, "keydown", function (e) {
-				key(e);
-				if (typeof e.keyCode === "number" && e.keyCode === 32) fn();
-			});
-			$.on(document, "keyup", function (e) { $(".click").removeClass("click"); });
-			$("#next").on("click", function (e) { next(e); });
-			$("#prev").on("click", function (e) { prev(e); });
-			$("#play").on("mousedown", fn);
-
-			// Setting up a data store
-			$.store(self);
-			self.on("afterDataSet", function(r) { load(r, false); }, "photo")
-			    .on("afterDataSync", function(data) {
-			    	config.data[config.loaded] = data.photo;
-			    	index = next();
-			    	delete abaaso.flickr.init;
-			    }, "photoset");
-
-			self.data.source = "photoset";
-			self.data.key    = "id";
-			self.data.uri    = uri;
-		};
-
-		/**
-		 * Keypress handler
-		 * 
-		 * @param {Object} e Keyboard event
-		 * @return {Object} Keyboard event
-		 */
-		key = function (e) {
-			var code = (e.keyCode) ? e.keyCode : e.charCode;
-
-			if (typeof $.timer.flickr !== "undefined") return;
-
-			switch (code) {
-				case 37:
-				case 40:
-					prev();
-					$("#prev").addClass("click");
-					break;
-				case 38:
-				case 39:
-					next();
-					$("#next").addClass("click");
-					break;
-			}
-			return e;
-		};
-
-		/**
-		 * Retrieves sizes for a photo from the set
-		 * 
-		 * @param  {Object}  photo Photo record to display
-		 * @param  {Boolean} show  [Optional] Defaults to true, will display the photo
-		 * @return {Object} Photo record to display
-		 */
-		load = function (photo, show) {
-			if (typeof photo === "undefined" || typeof photo.key === "undefined") return next();
-
-			show   = (show !== false);
-			var self  = $.flickr,
-			    uri   = "http://api.flickr.com/services/rest/?&method=flickr.photos.getSizes&api_key=" + config.key + "&photo_id=" + photo.key + "&format=json&jsoncallback=?",
-			    index = self.data.keys[photo.key].index,
-			    fn, r, img;
-
+		// Click & spacebar handler
+		fn = function () {
 			switch (true) {
-				case typeof photo.data.sizes === "undefined":
-					fn = function(arg) {
-						r = self.data.get(index);
-						r.data.sizes = arg.sizes.size.clone();
-						self.data.set(r.key, r.data, true);
-						switch (show) {
-							case true:
-								display(index);
-								break;
-							case false:
-								img = new Image();
-								img.src = r.data.sizes.last().source;
-						}
-					};
-					uri.jsonp(fn);
+				case typeof $.timer.flickr !== "undefined":
+					clearTimeout($.timer.flickr);
+					delete $.timer.flickr;
 					break;
-				case show:
-					display(index);
-					break;
-			}
-
-			return photo;
+				default:
+					$.timer.flickr = null;
+					next(); // @todo subvert this
+			};
 		};
 
-		/**
-		 * Displays the next image in the set
-		 * 
-		 * @param  {Object} e Window event
-		 * @return {Undefined} undefined
-		 */
-		next = function (e) {
-			var self = $.flickr,
-			    i    = (config.photo === null) ? Math.floor(Math.random() * self.data.records.length + 1) : parseInt(config.photo) + 1;
+		self.loaded = self.sets[i];
 
-			if (i > self.data.total) i = 0;
-			config.photo = i;
-			load(self.data.get(i));
-			return i;
+		// Setting up a data store
+		$.store(self);
+		self.on("afterDataSet",  function(r) { this.load(r, false); }, "photo")
+		    .on("afterDataSync", function(data) { this.data[this.loaded] = data.photo; }, "photoset");
+
+		self.data.source = "photoset";
+		self.data.key    = "id";
+		self.data.uri    = uri;
+	};
+
+	/**
+	 * Retrieves sizes for a photo from the set
+	 * 
+	 * @param  {Object}  photo Photo record to display
+	 * @param  {Boolean} show  [Optional] Defaults to true, will display the photo
+	 * @return {Object} Photo record to display
+	 */
+	flickr.prototype.load = function (photo, show) {
+		if (typeof photo === "undefined" || typeof photo.key === "undefined") return;
+
+		var self  = this,
+		    uri   = "http://api.flickr.com/services/rest/?&method=flickr.photos.getSizes&api_key=" + this.key + "&photo_id=" + this.key + "&format=json&jsoncallback=?",
+		    index = self.data.keys[photo.key].index,
+		    fn, r, img;
+
+		fn = function (arg) {
+			r = self.data.get(index);
+			r.data.sizes = arg.sizes.size.clone();
+			self.data.set(r.key, r.data, true);
+			if (show) self.fire("imageSizes", r.data.sizes);
 		};
 
-		/**
-		 * Displays the previous image in the set
-		 * 
-		 * @param  {Object} e Window event
-		 * @return {Undefined} undefined
-		 */
-		prev = function (e) {
-			var self = $.flickr,
-			    i = (config.photo === null) ? Math.floor(Math.random() * self.data.records.length + 1) : parseInt(config.photo) - 1;
+		if (typeof photo.data.sizes === "undefined") uri.jsonp(fn);
+		return photo;
+	};
 
-			if (i < 0 ) i = self.data.total - 1;
-			config.photo  = i;
-			load(self.data.get(i));
-			return i;
-		};
-
-		// @constructor
-		return {
-			config  : config,
-			display : display,
-			init    : init,
-			key     : key,
-			load    : load,
-			next    : next,
-			prev    : prev
-		};
-	}),
-	fn = function () { abaaso.module("flickr", flickr()); };
+	init = function (abaaso) {
+		$ = global[abaaso.aliased];
+		return abaaso.module("flickr", flickr);
+	};
 
 	// AMD support
-	typeof define === "function" ? define("abaaso.flickr", ["abaaso"], fn) : abaaso.on("render", fn, "abaaso.flickr");
-})(window);
+	typeof define === "function" ? define(["abaaso"], function (abaaso) { return init(abaaso); }) : abaaso.on("init", function () { return init(abaaso); }, "abaaso.flickr");
+})(this);
